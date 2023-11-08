2e7ee0e20384a2d16289ad94c0822da12d462d94
BrenoF013 <148352161+BrenoF013@users.noreply.github.com>
2023-11-08 10:42:11 -0300

    atualizado
    adicionamos a tabela de produtos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+const areaNovoproduto = document.querySelector('.areaNovoProduto');
+const escondido = document.querySelector('.block');  
+const codigo = document.querySelector('#txtCodigo');
+const nome = document.querySelector('#txtNome');
+const quantidade = document.querySelector('#txtQtd');    
+const valor = document.querySelector('#txtValor');   
+
 function verificarSituacaoEstoque(quantidade)
 {
     let quantidadeProduto = Number(quantidade);
@@ -31,36 +38,60 @@ const botao = document.querySelector ('.btnNovo');
 botao.addEventListener('click',function(e)
 {
     e.preventDefault()
-    const escondido = document.querySelector('.block');  
-    const areaNovoproduto = document.querySelector('.areaNovoProduto');
-    areaNovoproduto.classList.remove ("escondido");
+    areaNovoproduto.classList.remove("escondido");
     escondido.classList.remove("escondido");
-
-    
-    const codigo = document.querySelector('#txtCodigo');
-    const nome = document.querySelector('#txtNome');
-    const quantidade = document.querySelector('#txtQtd');    
-    const valor = document.querySelector('#txtValor');   
-    const salvar = document.querySelector('.salvarbtn'); 
-    const cancelar = document.querySelector('.cancelarbtn');
-    
-    salvar.addEventListener("click",function(e){
-        e.preventDefault()
-        console.log(nome.value, quantidade.value, valor.value);
-    });
-    
-    cancelar.addEventListener('click',function(e)
-    {
-        e.preventDefault()
-        areaNovoproduto.classList.add("escondido");
-        escondido.classList.add("escondido");
-    
-        codigo.value="";
-        nome.value="";
-        quantidade="";
-        valor="";
-    }
-    );
 }
 );
 
+const salvar = document.querySelector('.salvarbtn'); 
+salvar.addEventListener("click",function(e){
+    e.preventDefault()
+
+    const linhaNova = document.createElement("tr")
+    linhaNova.classList.add("linha-produto")
+
+    
+   
+    const linhaNovaCodigo = document.createElement("td")
+    const linhaNovaNome = document.createElement("td")
+    const linhaNovaQtd = document.createElement("td")
+    const linhaNovaValor = document.createElement("td")
+    const linhaNovaSituacao = document.createElement("td")
+
+    linhaNovaSituacao.innerHTML = verificarSituacaoEstoque(localQuantidade)
+
+
+    linhaNovaNome.textContent = nome.value
+    linhaNovaQtd.textContent = quantidade.value
+    linhaNovaValor.textContent = valor.value
+
+    const tabela = document.querySelector("#tabela")
+    tabela.appendChild(linhaNova)
+    linhaNova.appendChild(linhaNovaCodigo)
+    linhaNova.appendChild(linhaNovaNome)
+    linhaNova.appendChild(linhaNovaQtd)
+    linhaNova.appendChild(linhaNovaValor)
+    linhaNova.appendChild(linhaNovaSituacao)
+
+    areaNovoproduto.classList.add("escondido");
+    escondido.classList.add("escondido");
+
+    codigo.value="";
+    nome.value="";
+    quantidade.value="";
+    valor.value="";
+});
+
+const cancelar = document.querySelector('.cancelarbtn');
+cancelar.addEventListener('click',function(e)
+{
+    e.preventDefault()
+    areaNovoproduto.classList.add("escondido");
+    escondido.classList.add("escondido");
+
+    codigo.value="";
+    nome.value="";
+    quantidade.value="";
+    valor.value="";
+}
+);
